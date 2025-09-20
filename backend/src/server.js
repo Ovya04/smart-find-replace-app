1543580380210f0783d05b71c0a58794549e64dc
OvyaAnand <ovya4511@gmail.com>
2025-09-20 20:09:58 +0530

    server cors update

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,9 +10,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cors({
+  origin: ['https://app.contentstack.com', 'smart-find-replace-frontend.eu-contentstackapps.com'],
+  credentials: true,
+}));
+
 
 // MongoDB connection
 if (process.env.MONGODB_URI) {
